ad353f6c68b684e195470c57bd8105fd0e34d837
Nico <nicolasjouveaux@gmail.com>
2025-12-02 20:27:17 +0100

    Add scripts

---
+++ ressources/scripts/computer/informations/graphic_card.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#############################
+# Script graphic_card
+# 2025/11/29
+##############################
+
+echo "Détails de la carte graphique"
+
+# Checked if 'lshw' is installed
+if command -v lshw &> /dev/null
+then
+    echo "Recherche de la carte graphique"
+# Command execution
+    lshw -C display    
+else
+# Command isn't installed : error message
+    echo "ATTENTION : La commande 'lshw' n'est pas installée."
+fi
