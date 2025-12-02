0a44f11898a96111b34d39842c2dd2300061dd0b
Nico <nicolasjouveaux@gmail.com>
2025-12-02 20:24:25 +0100

    Add script firewall

---
+++ ressources/scripts/computer/informations/firewall.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#############################
+# Script firewall
+# 2025/12/01
+##############################
+
+# Afficher le status du pare-feu
+ufw status | grep -q "Status: active"
+if [ $? -eq 0 ]
+then
+    echo "Le pare-feu est activé."
+    read -p "Voulez-vous le désactiver ? (o/n) : " reply
+    if [ "$reply" = "o" ]
+    then
+        echo "Le pare-feu a été désactivé."
+    else
+        echo "Le pare-feu est toujours activé."
+    fi
+else
+    echo "Le pare-feu est désactivé."
+    read -p "Voulez-vous l'activer ? (o/n) : " reply
+    if [ "$reply" = "o" ]
+    then
+        echo "Le pare-feu a été activé."
+    else
+        echo "Le pare-feu est toujours désactivé."
+    fi
+fi
