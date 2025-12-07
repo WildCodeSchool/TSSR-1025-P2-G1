9f3aeefff8e132650d2deceaebfb002050a14baf
MATTHIAS CHICAUD <chicaud.matthias@gmail.com>
2025-12-07 12:39:12 +0100

    Add dady_script_V11, template_save_information, modification script_versio_os

--- ressources/scripts/template_save_information.sh
+++ ressources/scripts/template_save_information.sh
@@ -1,33 +1,35 @@
 #!/bin/bash
 
 #########################################################################
-# Script version os
-# Paisant Franck
-# 30/11/2025
+# Template save information
+# Chicaud Matthias
+# 07/12/2025
 #########################################################################
 
 #########################################################################
-#                     Define colors with variables                      #
+# INFORMATION
 #########################################################################
 
-# For menu titles: Underlined and yellow
-TITLE='\033[4;33m'
-# Used for labels: purple
-LABEL='\033[0;35m'
-# Used for FALSE: red
-RED='\033[0;31m'
-# Used for TRUE: green
-GREEN='\033[0;32m'
-# Reset color at end of line
-NC='\033[0m'
-#########################################################################
+# 1. Copy the VARIABLE section into your child script.
+# 2. CHOOSE ONE OPTION:
+#       USER: info_target="wilder"
+#       COMPUTER: info_target=$(hostname)
+# 3. Copy the save_info() function into the FUNCTIONS section.
+# 4. Retrieve the information.
+# 5. Format this information into value.
+# 6. Call save_info "Name of information" "$value".
+
+# The generated file will be: info_<target>_<yyyymmdd>.txt
+# Always use >> to add information to the same file for the day.
 
 #########################################################################
 # Variable
 #########################################################################
 
 # Variable for save_info function
-info_target="$(hostname)"
+
+# info_target="wilder" # Uncomment for user script
+# info_target="$(hostname)" # Uncomment for computer script
 info_date="$(date +%Y%m%d)"
 info_dir="/tmp/info"
 info_file="$info_dir/info_${info_target}_${info_date}.txt"
@@ -36,30 +38,25 @@ info_file="$info_dir/info_${info_target}_${info_date}.txt"
 # Function
 #########################################################################
 
-# function for save information in file
+# Function for save information in file
 save_info()
 {
     local label="$1"
     local value="$2"
     local time_save_info="$(date +%H:%M:%S)"
-    mkdir -p $info_dir
-    echo "[$time_save_info] $label : $value" >> $info_file
+    mkdir -p "$info_dir"
+    echo "[$time_save_info] $label : $value" >> "$info_file"
 }
 
 #########################################################################
 # Script
 #########################################################################
 
-# menu name display
-    echo -e "${TITLE}Version de l'OS${NC}"
-    echo ""
+# Example:
+# value="$(lsb_release -ds 2>/dev/null)"
+# save_info "Version de l'OS" "$value"
 
-#command to display the OS version
-value="$(lsb_release -ds 2>/dev/null)"
-    echo -e "$value"
-    echo ""
-
-    save_info "Version de l'OS" "$value"
-exit 0
+value="raw_value"
+save_info "Name of information" "$value"
 
 #########################################################################
