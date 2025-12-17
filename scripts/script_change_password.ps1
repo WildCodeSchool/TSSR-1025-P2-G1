3f18451d921d5dd24dce7789b8b6546799a197ba
MATTHIAS CHICAUD <chicaud.matthias@gmail.com>
2025-12-18 00:13:44 +0100

    MaJ Script kids

--- scripts/script_change_password.ps1
+++ scripts/script_change_password.ps1
@@ -23,27 +23,48 @@ $OutputEncoding           = [System.Text.UTF8Encoding]::new($false)
     Write-Host "Changement du mot de passe d'un utilisateur" -ForegroundColor Yellow
     Write-Host ""
 
-    $user = Read-Host "Modifier le mot de pass sur quel utilisateur"
-    
-$exists = Get-LocalUser -Name $user -ErrorAction SilentlyContinue
-    if ( $exists )
-    {
-        Write-Host "WARNING : L'utilisateur '$user' existe déjà." -Foregroundcolor Red
-        exit 1
-    }
-    try
-    {
-        New-LocalUser -Name $user -NoPassword -ErrorAction Stop | Out-Null
-        Write-Host "L'utilisateur '$user' a été créé" -ForegroundColor Green
-        
-    }
-    catch
-    {
-        Write-Host "WARNING : L'utilisateur '$user' n'a pas été créé !!!" -ForegroundColor Red
-        Write-Host ""
-        exit 1
-    }
-exit 0
+# User
+while ($True) {
+    # Input of the user name
+    $user = Read-Host "Nom de l'utilisateur dont vous voulez changer le mot de passe"
 
+    # Verify if user exist
+    try {
+        Get-LocalUser -Name $user -ErrorAction Stop | Out-Null
+        Write-Host "Utilisateur trouvé : $user" -ForegroundColor Green
+        Write-Host ""
+        break
+    }
+    catch {
+        Write-Host "WARNING ! l'utilisateur $user n'existe pas." -ForegroundColor Red
+    }
+}
+
+Write-Host "Voulez-vous vraiment changer le mot de passe de $user"
+$confirm = Read-Host "Confirmez-vous cette action ? (o/n)"
+
+if ($confirm -notmatch '^[oO]$'){
+    Write-Host "Opération annulée." -ForegroundColor Red
+    exit 0
+}
+
+Write-Host ""
+
+# change password
+$new_password = Read-Host "Entrez le nouveau mot de passe" -AsSecureString
+
+try {
+    Set-LocalUser -Name $user -Password $new_password -ErrorAction Stop
+    Write-Host ""
+    Write-Host "Le mot de passe de $user a été modifié avec succès !" -ForegroundColor Green
+    Write-Host ""
+    exit 0
+}
+catch {
+    Write-Host ""
+    Write-Host "WARNING ! Une erreur est survenue lors du changement de mot de passe" -ForegroundColor Red
+    Write-Host ""
+    exit 1
+}
 
 #############################################################################
